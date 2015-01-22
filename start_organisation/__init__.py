76f2ea9097664a9bba31a117caa29beb279b85df
Adam Shimali <adam.shimali@gmail.com>
2015-01-22 22:49:33 +0000

    redis connection that works locally and heroku

--- start_organisation/__init__.py
+++ start_organisation/__init__.py
@@ -8,8 +8,10 @@ app = Flask(__name__)
 app.config.from_object(os.environ.get('SETTINGS'))
 oauth = OAuth(app)
 
-import redis
-redis_client = redis.from_url(app.config.get('REDISCLOUD_URL', 'redis://user:@localhost:6379'))
+from redis import Redis
+import urllib
+url = urllib.parse.urlparse(app.config.get('REDISCLOUD_URL', 'redis://user:@localhost:6379'))
+redis_client = Redis(host=url.hostname, port=url.port, password=url.password)
 
 if 'SENTRY_DSN' in os.environ:
     from raven.contrib.flask import Sentry
